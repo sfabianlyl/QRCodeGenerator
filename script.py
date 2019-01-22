9a1dc7e1546ad1f2a9d593e59cac166c3ab0ae9f
sfabianlyl <36915473+sfabianlyl@users.noreply.github.com>
2019-01-22 11:32:19 +0800

    Add files via upload

---
+++ script.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 22 10:46:15 2019
+
+@author: User
+"""
+
+import pyqrcode
+from PIL import Image
+
+#variable definitions
+logoWidth=100       #change this to enlarge/reduce logo size
+string='https://www.facebook.com/HolyFamilyKajang/'         #url to be embedded
+logoString='logo.png'       #path to intended logo location
+out='qrcode.jpg'        #save name and format as jpg
+
+
+#generate qr code, open, obtain width and height of qrcode
+url=pyqrcode.QRCode(string)
+url.png('test.png',scale=10) #save qrcode as test.png
+im=Image.open('test.png') #open saved qrcode
+im=im.convert("RGBA")
+width,height = im.size
+
+
+#open logo image, crop and paste logo
+logo=Image.open(logoString)
+leftUpBorder = int((width-logoWidth)/2)
+rightBottomBorder = int((width+logoWidth)/2)
+box=(leftUpBorder,leftUpBorder,rightBottomBorder,rightBottomBorder) #logo placement. take (qrcode size-logo size)/2 , (qrcode size+logo size)/2
+im.crop(box)
+logo= logo.resize((logoWidth,logoWidth)) #set logo size
+im.paste(logo,box) #place logo
+#im.show()
+im.save(out)
